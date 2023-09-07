3d02c1c62f32fb8a5bc0bc5285a71664c8b2093c
Joshua Hebert <jhebertcocktails@gmail.com>
2023-09-06 21:58:49 -0400

    added app.js and index.js from previous repo

--- jhebert-react-portfolio/src/App.js
+++ jhebert-react-portfolio/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// Import React and the APP css file
+import React from "react";
+import "./App.css";
+// Import Components to use in App
+import NavBar from "./components/navBar/NavBar";
+import CustomCarousel from "./components/carousel/Carousel";
+import HomeMessage from "./components/home-message/HomeMessage.Js"
+// Import built in React components to use in App
+import Fade from "react-reveal/Fade";
+import * as reactParallax from "react-parallax";
+import Slide from "react-reveal/Slide";
+// Import View folders to use in App
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import ContactForm from "./components/contact-form/ContactForm";
+import Resume from "./components/resume/Resume";
+import Footer from "./components/footer/Footer";
+// Helper function to render section animations
+function SectionAnimation({ animationType, content }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="section-container rounded">
+        {/* Render the animation with content */}
+        {animationType && React.createElement(animationType, { duration: 1000, distance: "20px" }, content)}
+      </div>
     </div>
   );
 }
+// App function to render the App
+function App() {
+  const sections = [
+    { component: NavBar },
+    { component: CustomCarousel },
+    { component: HomeMessage },
+    {
+      component: (
+        <reactParallax.Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallax/background.jpg")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+            <div className="section-container rounded">
+              <Fade left duration={1000} distance="20px">
+                <About />
+              </Fade>
+            </div>
+          </div>
+        </reactParallax.Parallax>
+      ),
+    },
+    { views: Skills, animationType: Fade },
+    { views: Portfolio, animationType: Slide },
+    { views: Resume, animationType: Slide },
+    { views: Contact, animationType: Fade },
+    { views: ContactForm, animationType: Fade },
+    { views: Footer, animationType: Fade },
+  ];
 
-export default App;
+  return (
+    <div className="App"
+      style={{ position: "relative" }}>
+      {sections.map((section, index) => (
+        <SectionAnimation key={index}
+          animationType={section.animationType}
+        >
+          {section.component && <section.component />}
+          {index === sections.length - 2 && <hr />}
+        </SectionAnimation>
+      ))}
+    </div>
+  );
+}
+export default App; 
